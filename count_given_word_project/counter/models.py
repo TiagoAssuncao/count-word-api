d93882e8adfcce6e52491283d5386f6317ce7d72
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-26 09:32:06 -0300

    Adding model field count
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- count_given_word_project/counter/models.py
+++ count_given_word_project/counter/models.py
@@ -4,6 +4,7 @@ class Counter(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     word = models.CharField(max_length=100, blank=True)
     web_page = models.TextField()
+    count = models.PositiveIntegerField()
 
     class Meta:
         ordering = ('created',)
