c7a5d18b0dc03e104398e89ba6e8668b126693e4
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-26 09:43:17 -0300

    Change contrains to model counter
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- count_given_word_project/counter/models.py
+++ count_given_word_project/counter/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Counter(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    word = models.CharField(max_length=100, blank=True)
+    word = models.CharField(max_length=100)
     web_page = models.TextField()
-    count = models.PositiveIntegerField()
+    count = models.PositiveIntegerField(null=True,blank=True)
 
     class Meta:
         ordering = ('created',)
